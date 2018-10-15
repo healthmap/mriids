8dc2ea0a86fdc6aa2d4248078e00ba9239d4d6bb
Katelynn O'Brien <katelynn.obrien@childrens.harvard.edu>
2018-10-15 15:45:37 -0400

    refine some of the nav styling

--- src/App.js
+++ src/App.js
@@ -6,12 +6,25 @@ class App extends Component {
     return (
       <div className="app">
         <div className="sidebar">
-          sidebar here
+          <div className="search">Search</div>
+          <div className="outbreak">Outbreak</div>
+          <div className="reported-cases">Reported Cases</div>
         </div>
         <header className="header">
-          header here
+          <div className="logo">logo</div>
+          <ul className="nav nav--inline">
+            <li className="is-active">Outbreak</li>
+            <li>Historic</li>
+            <li>About</li>
+          </ul>
+          <div className="nav nav--inline">icon nav</div>
+          <div className="nav nav--inline">icon nav</div>
         </header>
         <div className="map">
+          <ul class="nav nav--inline nav--toggle">
+            <li>Snapshot</li>
+            <li>Risk</li>
+          </ul>
           map here
         </div>
         <div className="epicurve">
