65ce6b36de439b5c9d5f419f04bfd29e1797c3b3
Katelynn O'Brien <katelynn.obrien@childrens.harvard.edu>
2018-10-16 09:26:41 -0400

    mriids logo, separate stylesheets for main areas

--- src/App.js
+++ src/App.js
@@ -12,19 +12,21 @@ class App extends Component {
           <div className="reported-cases">Reported Cases</div>
         </div>
         <header className="header">
-          <div className="logo">logo</div>
+          <div className="logo">
+            <img src="/mriids.svg" alt="mriids" />
+          </div>
           <ul className="nav nav--inline">
-            <li className="is-active">Outbreak</li>
-            <li>Historic</li>
-            <li>About</li>
+            <li className="is-active"><a href="">Outbreak</a></li>
+            <li><a href="">Historic</a></li>
+            <li><a href="">About</a></li>
           </ul>
           <div className="nav nav--inline">icon nav</div>
           <div className="nav nav--inline">icon nav</div>
         </header>
         <div className="map">
           <ul class="nav nav--inline nav--toggle">
-            <li>Snapshot</li>
-            <li>Risk</li>
+            <li><a href="">Snapshot</a></li>
+            <li><a href="">Risk</a></li>
           </ul>
           map here
         </div>
