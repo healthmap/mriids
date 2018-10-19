eac89e1f6b88b033dacb01019aa21ffe70ff36d3
Katelynn O'Brien <katelynn.obrien@childrens.harvard.edu>
2018-10-19 19:07:19 -0400

    move logo to component, adjust header and reported cases styles

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import HeaderNav from './HeaderNav/HeaderNav';
+import Logo from '../../Logo/Logo';
+import './Header.scss';
 
 class Header extends Component {
    render() {
       return (
         <header className="header">
-          <div className="logo">
-            <img src="/mriids.svg" alt="mriids" />
-          </div>
+          <Logo />
           <HeaderNav />
           <div className="nav nav--inline">icon nav</div>
           <div className="nav nav--inline">icon nav</div>
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Logo.scss';
+
+const Logo = ({}) => (
+  <div className="logo">
+    <img src="/mriids.svg" alt="mriids" />
+  </div>
+);
+
+export default Logo;
