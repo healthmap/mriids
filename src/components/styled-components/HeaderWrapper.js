39a2344fcf57d7076df2cc59eb9264ea32dbb142
Katelynn O'Brien <katelynn.obrien@childrens.harvard.edu>
2018-10-29 10:24:46 -0400

    prevent header from collapsing when window is short

--- src/components/styled-components/HeaderWrapper.js
+++ src/components/styled-components/HeaderWrapper.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 const Header = styled.div`
   position: relative;
   display: flex;
+  flex: none;
   width: 100%;
   height: 4.5rem; /* $header-height */
   box-shadow: 0 10px 12px -8px rgba(0,0,0,0.10); /* $box-shadow--bottom */
