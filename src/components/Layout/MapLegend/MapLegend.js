aab7066700a3c5a61816f74f8167b661e60e936e
Katelynn O'Brien <katelynn.obrien@childrens.harvard.edu>
2018-11-06 17:59:26 -0500

    remove key attr from maplegend

--- src/components/Layout/MapLegend/MapLegend.js
+++ src/components/Layout/MapLegend/MapLegend.js
@@ -4,9 +4,7 @@ import './MapLegend.scss'
 function MapLegend(props) {
   return (
     <div className="map-legend-item">
-      <div className="map-legend-item-color" key={props.key}
-        style={{backgroundColor: props.color}}>
-      </div>
+      <div className="map-legend-item-color" style={{backgroundColor: props.color}}></div>
       <div className="map-legend-item-value">
         {props.value}
       </div>
